0c2e823f12155ab89bf4319410bae37531ce42b4
Arran Ewing <azzerx@gmail.com>
2019-11-01 12:36:37 +0000

    Add FindSequenceCommand and Tests

---
+++ WordSequenceFinder.Core/FindSequence/FindSequenceCommand.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace WordSequenceFinder.Core.FindSequence
+{
+    public class FindSequenceCommand : AbstractValidator<FindSequenceCommand>
+    {
+        public string DictionaryLocation { get; set; }
+        public string StartWord { get; set; }
+        public string EndWord { get; set; }
+        public string ResultLocation { get; set; }
+    }
+}
---
+++ WordSequenceFinder.Core/FindSequence/FindSequenceCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace WordSequenceFinder.Core.FindSequence
+{
+    public class FindSequenceCommandValidator : AbstractValidator<FindSequenceCommand>
+    {
+        public FindSequenceCommandValidator()
+        {
+            RuleFor(cmd => cmd.DictionaryLocation)
+                                .NotNull()
+                                .MinimumLength(1);
+            RuleFor(cmd => cmd.StartWord)
+                                .NotNull()
+                                .Length(4);
+            RuleFor(cmd => cmd.EndWord)
+                                .NotNull()
+                                .Length(4);
+            RuleFor(cmd => cmd.ResultLocation)
+                                .NotNull()
+                                .MinimumLength(1);
+        }
+    }
+}
